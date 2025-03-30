2fee76eebfcfec620432e1ecb21c7c09d14c6c23
Rahul Dey <rahuldey832000@gmail.com>
2025-03-30 16:57:28 +0530

    Add number input and only one character input handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,62 @@
+import { useState } from "react";
+
+const OTP_DIGITS_COUNT = 6;
+const defaultArray = new Array<string>(OTP_DIGITS_COUNT).fill("");
+
 function App() {
+  const [inputArr, setInputArr] = useState<string[]>(defaultArray);
+
+  const handleOnChange = (value: string, index: number) => {
+    try {
+      console.log({ value });
+      const trimmedValue = value.trim();
+
+      if (trimmedValue.length === 0) {
+        return;
+      }
+
+      const lastChar = trimmedValue.slice(-1);
+
+      if (isNaN(Number.parseInt(lastChar))) {
+        return;
+      }
+
+      const newInputArr = [...inputArr];
+      newInputArr[index] = lastChar;
+      setInputArr(newInputArr);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleKeyPress = (e: any, index: number) => {
+    const key = e.key;
+
+    if (key !== "Backspace") {
+      return;
+    }
+  };
+
   return (
-    <>
+    <div className="app">
       <h1>OTP Input</h1>
-      
-    </>
+      <div className="otp-container">
+        {inputArr.map((input, index) => {
+          return (
+            <input
+              key={index}
+              className="otp-input"
+              type="text"
+              name="otp"
+              id="otp"
+              value={input}
+              onChange={(e) => handleOnChange(e.target.value, index)}
+              onKeyDown={(e) => handleKeyPress(e, index)}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
