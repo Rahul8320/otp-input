0127f467d3c6dae38650e8cd6699ba966ae1a67c
Rahul Dey <rahuldey832000@gmail.com>
2025-03-30 18:29:06 +0530

    Add auto next and previous of backspace changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const OTP_DIGITS_COUNT = 6;
 const defaultArray = new Array<string>(OTP_DIGITS_COUNT).fill("");
@@ -6,42 +6,51 @@ const defaultArray = new Array<string>(OTP_DIGITS_COUNT).fill("");
 function App() {
   const [inputArr, setInputArr] = useState<string[]>(defaultArray);
 
+  const refInputArr = useRef<any[]>([]);
+
   const handleOnChange = (value: string, index: number) => {
     try {
-      console.log({ value });
+      if (isNaN(value as any)) {
+        return;
+      }
+
       const trimmedValue = value.trim();
-
-      if (trimmedValue.length === 0) {
-        return;
-      }
-
       const lastChar = trimmedValue.slice(-1);
-
-      if (isNaN(Number.parseInt(lastChar))) {
-        return;
-      }
 
       const newInputArr = [...inputArr];
       newInputArr[index] = lastChar;
       setInputArr(newInputArr);
+
+      if (lastChar.length > 0) {
+        refInputArr.current[index + 1]?.focus();
+      }
     } catch (err) {
       console.error(err);
     }
   };
 
-  const handleKeyPress = (e: any, index: number) => {
+  const handleOnKeyDown = (e: any, index: number) => {
     const key = e.key;
+    console.log({ index });
 
     if (key !== "Backspace") {
       return;
     }
+
+    if (!e.target.value) {
+      refInputArr.current[index - 1]?.focus();
+    }
   };
+
+  useEffect(() => {
+    refInputArr.current[0]?.focus();
+  }, []);
 
   return (
     <div className="app">
-      <h1>OTP Input</h1>
+      <h1>Please input your OTP</h1>
       <div className="otp-container">
-        {inputArr.map((input, index) => {
+        {inputArr.map((_, index) => {
           return (
             <input
               key={index}
@@ -49,9 +58,10 @@ function App() {
               type="text"
               name="otp"
               id="otp"
-              value={input}
+              value={inputArr[index]}
               onChange={(e) => handleOnChange(e.target.value, index)}
-              onKeyDown={(e) => handleKeyPress(e, index)}
+              onKeyDown={(e) => handleOnKeyDown(e, index)}
+              ref={(input: any) => (refInputArr.current[index] = input)}
             />
           );
         })}
